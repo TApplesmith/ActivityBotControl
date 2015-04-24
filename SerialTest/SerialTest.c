7c8abf6655da123c121d15d6804110e08755ce33
DangerousElectrician <DangerousElectrician@gmail.com>
2015-04-24 16:49:22 -0500

    changed the way serial terminals are handled

--- SerialTest/SerialTest.c
+++ SerialTest/SerialTest.c
@@ -1,6 +1,6 @@
 
 #include "simpletools.h"                      // Include simpletools header
-
+#include "fdserial.h"
 
 #define RX_PIN 31
 #define TX_PIN 30
@@ -9,15 +9,22 @@
 
 int main()                                    // main function
 {
-  simpleterm_reopen(RX_PIN, TX_PIN, 0, BAUD);
+  simpleterm_close(); //close default terminal, I want to use those pins
+  fdserial *wireless = fdserial_open(RX_PIN, TX_PIN, 0, BAUD); //this will go to the bluetooth module eventually
   
-  while(1)                                    // Repeat indefinitely
+  int t = 0;
+  while(scan("%d",&t)==0){} //wait for serial input
+  
+  int n = 0;
+  while(1)                                    
   {
-    for(int i = 0; i < 255; i++) 
-    {
-      //print("%d\n", i);
-      putChar(1000);
-      pause(50);
-    }    
+    int i = n;
+    //printi("%d %d %d\n",i,i>>8,(char) i);
+    writeChar(wireless, (char)(i>>24));
+    writeChar(wireless, (char)(i>>16));
+    writeChar(wireless, (char)(i>>8));
+    writeChar(wireless, (char)i);
+    n++; 
+    pause(2);
   }
 }
