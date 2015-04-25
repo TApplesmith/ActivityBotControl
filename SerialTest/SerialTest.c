1ee7ff5b6559b92d819c96856a5c5dec0e7ae7ee
DangerousElectrician <DangerousElectrician@gmail.com>
2015-04-25 00:46:16 -0500

    made this work again

--- SerialTest/SerialTest.c
+++ SerialTest/SerialTest.c
@@ -12,18 +12,33 @@ int main()
   simpleterm_close(); //close default terminal, I want to use those pins
                       //if the default uart pins are not used, this isn't needed
                       
-  startComs(RX_PIN, TX_PIN, BAUD); //this will go to the bluetooth module eventually
+  startComs(RX_PIN, TX_PIN, BAUD, 1000); //this will go to the bluetooth module eventually
   
-  rxCharblock(); //wait for a control byte
   
   int n = 0;
   while(1)                                    
   {
-    int i = n;
-    sendInt32(n); //sending incrementing numbers
-    n+=10000; 
-    //the computer can sometimes miss bytes 
+    
+    switch(rxCommand()) //wait for a control byte
+    {
+      case 'a':
+        sendInt32(n); //sending incrementing numbers
+        n+=10000; 
+        break;
+        
+      case '?':
+        sendInt32(42);
+        break;
+        
+      case -1:    //case when timed out
+        sendInt32(314);
+        break;
+        
+      default:    //unknown command
+        sendInt32(2718);
+    }        
+    
+    //Note: The computer can sometimes miss bytes 
     //when the bytes are coming really fast 
-    //for an extended period of time
   }
 }
