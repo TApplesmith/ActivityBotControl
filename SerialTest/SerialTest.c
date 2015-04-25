ae1e4c5685f40ea6b5c44e28c35b1a2837cbdad2
DangerousElectrician <DangerousElectrician@gmail.com>
2015-04-24 20:54:36 -0500

    refactored

--- SerialTest/SerialTest.c
+++ SerialTest/SerialTest.c
@@ -6,9 +6,15 @@
 #define TX_PIN 30
 #define BAUD 115200
 
+void sendInt32(fdserial* connection, int i)
+{
+  writeChar(connection, (char)i); //little endian
+  writeChar(connection, (char)(i>>8));
+  writeChar(connection, (char)(i>>16));
+  writeChar(connection, (char)(i>>24));
+}    
 
-
-int main()                                    // main function
+int main()
 {
   simpleterm_close(); //close default terminal, I want to use those pins
   fdserial *wireless = fdserial_open(RX_PIN, TX_PIN, 0, BAUD); //this will go to the bluetooth module eventually
@@ -19,10 +25,7 @@ int main()                                    // main function
   while(1)                                    
   {
     int i = n;
-    writeChar(wireless, (char)i); //little endian
-    writeChar(wireless, (char)(i>>8));
-    writeChar(wireless, (char)(i>>16));
-    writeChar(wireless, (char)(i>>24));
+    sendInt32(wireless, n);
     n+=10000; 
     pause(2);
   }
