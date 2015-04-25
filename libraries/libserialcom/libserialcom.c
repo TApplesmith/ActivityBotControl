c79d8acc570cb78fc3e00e706bba48ead04eaa5b
DangerousElectrician <DangerousElectrician@gmail.com>
2015-04-24 23:46:36 -0500

    serial communications library

---
+++ libraries/libserialcom/libserialcom.c
@@ -0,0 +1,26 @@
+
+#include "serialcom.h"
+
+#define RX_PIN 31
+#define TX_PIN 30
+#define BAUD 115200
+
+
+int main()
+{
+  simpleterm_close(); //close default terminal, I want to use those pins
+                      //if the default uart pins are not used, this isn't needed
+                      
+  startComs(RX_PIN, TX_PIN, BAUD); //this will go to the bluetooth module eventually
+  
+  rxCharblock(); //wait for a control byte
+  
+  int n = 0;
+  while(1)                                    
+  {
+    sendInt32(n); //sending incrementing numbers
+    n+=10000; 
+    //Note: The computer can sometimes miss bytes 
+    //when the bytes are coming really fast 
+  }
+}
---
+++ libraries/libserialcom/serialcom.h
@@ -0,0 +1,8 @@
+
+#include "fdserial.h"
+
+void startComs(int rxpin, int txpin, int baudrate);
+
+void sendInt32(int i);
+
+char rxCharblock();
