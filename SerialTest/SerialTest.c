d4f128ba73c8b7350d2f6feb13304b7f2db19211
DangerousElectrician <DangerousElectrician@gmail.com>
2015-04-24 19:56:28 -0500

    sends ints as separate bytes now

--- SerialTest/SerialTest.c
+++ SerialTest/SerialTest.c
@@ -7,24 +7,23 @@
 #define BAUD 115200
 
 
+
 int main()                                    // main function
 {
   simpleterm_close(); //close default terminal, I want to use those pins
   fdserial *wireless = fdserial_open(RX_PIN, TX_PIN, 0, BAUD); //this will go to the bluetooth module eventually
   
-  int t = 0;
-  while(scan("%d",&t)==0){} //wait for serial input
+  fdserial_rxChar(wireless);
   
   int n = 0;
   while(1)                                    
   {
     int i = n;
-    //printi("%d %d %d\n",i,i>>8,(char) i);
-    writeChar(wireless, (char)(i>>24));
-    writeChar(wireless, (char)(i>>16));
+    writeChar(wireless, (char)i); //little endian
     writeChar(wireless, (char)(i>>8));
-    writeChar(wireless, (char)i);
-    n++; 
+    writeChar(wireless, (char)(i>>16));
+    writeChar(wireless, (char)(i>>24));
+    n+=10000; 
     pause(2);
   }
 }
