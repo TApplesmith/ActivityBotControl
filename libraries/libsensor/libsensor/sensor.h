0a82b388c1d2c7a57eee1aa4fc3197c732b071ec
TApplesmith <sgf3211@gmail.com>
2015-05-01 17:33:59 -0500

    Re-Archived Library after realized didn't commit
    Re-Archived Library after realized didn't commit properly

--- libraries/libsensor/libsensor/sensor.h
+++ libraries/libsensor/libsensor/sensor.h
@@ -1,2 +1,15 @@
 //sensor.h
+//get ping [over microseconds]
 #include "simpletools.h"
+#include "ping.h" // Include ping header
+
+#define PINGPIN 8 //Ping in pin 8
+
+int getPing();
+{
+  return ping(PINGPIN); // return microseconds distance from ping sensor
+}
+
+// ARCHIVED
+// probably going to be incorporated into wander in order
+// to avoid error due to movement
